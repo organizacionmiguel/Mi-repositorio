0440d66cacfbf4d440bb5d9b24eb5b6d64b6a4e2
Trampa <misterbryn99@gmail.com>
2019-05-22 12:39:53 +0200

    clase Persona

---
+++ src/Persona.java
@@ -0,0 +1,47 @@
+
+public class Persona {
+	private String nombre;
+	private String sexo;
+	private int edad;
+	
+	
+	public Persona(String nombre, String sexo, int edad) {
+		super();
+		this.nombre = nombre;
+		this.sexo = sexo;
+		this.edad = edad;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getSexo() {
+		return sexo;
+	}
+
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+	
+}
