affcdfd2d4619caba5bfd91e93faef0ddef84ad4
The-Kaos99 <marian.somesan.smr@gmail.com>
2019-05-27 13:26:54 +0200

    Añadido toString

--- src/Persona.java
+++ src/Persona.java
@@ -1,10 +1,18 @@
+	
 
 public class Persona {
+	
+
 	private String nombre;
+	
+
 	private String sexo;
+	
+
 	private int edad;
 	
 	
+
 	public Persona(String nombre, String sexo, int edad) {
 		super();
 		this.nombre = nombre;
@@ -12,34 +20,39 @@ public class Persona {
 		this.edad = edad;
 	}
 
-
+	
 	public String getNombre() {
 		return nombre;
 	}
 
-
+	
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
-
+	
 	public String getSexo() {
 		return sexo;
 	}
 
-
+	
 	public void setSexo(String sexo) {
 		this.sexo = sexo;
 	}
 
-
+	
 	public int getEdad() {
 		return edad;
 	}
 
-
+	
 	public void setEdad(int edad) {
 		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", sexo=" + sexo + ", edad=" + edad + "]";
 	}
 	
 	
